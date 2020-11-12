4d07aa930b233cd296dedbfed1e071fc6098b7c1
Fem Adisa <femiwonderful@gmail.com>
2020-11-12 08:22:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,13 @@
-def using_concats(array, second_array)
+def using_concat(array, second_array)
   array.concat(second_array)
 end
 
-def 
+def using_insert(array, element)
+  array.insert(4 = element
+end
+
+def using_uniq (array)
+  array.uniq 
+end
+
+def
